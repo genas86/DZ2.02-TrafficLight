51d9317eae56d496289e087e672d288e4c5acb10
genas86 <sgm86@bk.ru>
2022-07-29 15:16:26 +0300

    Initial Commit

---
+++ DZ2.02-TrafficLight/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DZ2.02-TrafficLight
+//
+//  Created by Genas1986 on 29.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
